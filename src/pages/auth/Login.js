2d56512957a85c11dd074d188a0b93cad30ce068
tjgus <0226zzang@gmail.com>
2026-05-20 15:15:08 +0900

    Update Login.js

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -48,9 +48,9 @@ function Login(props) {
           <h2>LOGIN</h2>
 
           <ul className="social-login">
-            <li><Link to="/auth/naver"><img src={process.env.PUBLIC_URL + '/images/naver.jpg'} alt="네이버" /></Link></li>
-            <li><Link to="/auth/kakao"><img src={process.env.PUBLIC_URL + '/images/kakao.jpg'} alt="카카오"/></Link></li>
-            <li><Link to="/auth/google"><img src={process.env.PUBLIC_URL + '/images/google.jpg'} alt="구글"/></Link></li>
+            <li><Link to="/auth"><img src={process.env.PUBLIC_URL + '/images/naver.jpg'} alt="네이버" /></Link></li>
+            <li><Link to="/auth"><img src={process.env.PUBLIC_URL + '/images/kakao.jpg'} alt="카카오"/></Link></li>
+            <li><Link to="/auth"><img src={process.env.PUBLIC_URL + '/images/google.jpg'} alt="구글"/></Link></li>
           </ul>
 
           <form className='login_form' onSubmit={handleSubmit}>
@@ -64,7 +64,7 @@ function Login(props) {
 
               <div className="password-wrapper">
                 <input
-                  type={show ? "text" : "password"}   // ✅ 여기!
+                  type={show ? "text" : "password"}  
                   name="user_pw"
                   id="password"
                   placeholder="비밀번호를 입력하세요."
